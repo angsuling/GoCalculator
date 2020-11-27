d1eead7343d5a9e5bce90cad5cae86e761f501d8
angsuling <55497556+angsuling@users.noreply.github.com>
2020-11-27 15:28:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
-package main
+package calc
 
-import "fmt"
+//Add adds 2 integers and returns an integer
+func Add(x, y int) int {
+	return x + y
+}
 
-func main() {
-	fmt.Println("Testing!")
+//Subtract subtract second integer from the first
+func Subtract(x, y int) int {
+	return x - y
+}
+
+//Divide divide second integer from the first
+func Divide(x, y int) float64 {
+	return float64(x) / float64(y)
+}
+
+//Multiply multiplies x by y
+func Multiply(x, y int) int {
+	return x * y
 }
